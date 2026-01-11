b01645da7e715f577a6f99d541d293672b75cbc6
JoyfulRak <star3858@gmail.com>
2026-01-11 23:45:21 +0900

    feat(project):이름입력구현

--- Data/GameManager.cs
+++ Data/GameManager.cs
@@ -36,6 +36,31 @@ public class GameManager
         //타이틀 표시
         ConsoleUI.showTitle();
         Console.WriteLine("빡센게임에 오신걸 환영합니다.");
+        
+        //TODO:캐릭터 생성
+        CreateCharacter();
+        //TODO:인벤토리 초기화
+        //TODO:초기 아이템 지급
+
+    }
+    
+
+    #endregion
+
+    #region 캐릭터 생성
+
+    private void CreateCharacter()
+    {
+        //이름 입력
+        Console.WriteLine("캐릭터의 이름을 입력하세요");
+        String? name=Console.ReadLine();//nullable 허용
+
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            name = "무명용사";
+            
+        }
+        Console.WriteLine($"{name}님, 모험을 시작하겠습니다!");
     }
 
     #endregion
